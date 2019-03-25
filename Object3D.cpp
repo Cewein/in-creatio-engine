787f90e0315c1e0dee846e7065ff62c75335648a
Cewein <pro.mnowak@gmail.com>
2019-03-25 22:42:29 +0100

    object3D is full of craps
    need to redo the class, actual bullshit going on here as of now

--- Object3D.cpp
+++ Object3D.cpp
@@ -1,12 +1,20 @@
 #include "Object3D.h"
 
 
-
-Object3D::Object3D()
+Object3D::Object3D(double * vecArray, int nbOfFaces)
 {
-}
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 
+	glBindVertexArray(VAO);
 
-Object3D::~Object3D()
-{
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8 * nbOfFaces, vecArray, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 }
--- Object3D.h
+++ Object3D.h
@@ -1,19 +1,21 @@
 #pragma once
+#include <glad/glad.h>
 class Object3D
 {
 private:
-	size_t VOA;
+	size_t VAO;
 	size_t VBO;
+
+	int numberOfFaces;
 
 
 public:
 
-	Object3D();
+	Object3D(double * vecArray, int nbOfFaces);
 
 	//getter
 
-	inline size_t getVOA() { return VOA; }
+	inline size_t getVOA() { return VAO; }
 	inline size_t getVBO() { return VBO; }
-	~Object3D();
 };
 
